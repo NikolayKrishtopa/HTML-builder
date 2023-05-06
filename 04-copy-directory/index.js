02aa9c018fd77d75348f7ad26479c4e572740fd1
Nikolay Krishtopa <nikolay.krishtopa@gmail.com>
2023-05-06 12:34:05 +0400

    feat: finish 04-copy-files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir } = require('node:fs/promises');
 
+const createDirCopy = async () => {
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) throw err;
+    console.log('Папка была создана');
+    copyFiles();
+  });
+};
+
+const copyFiles = async () => {
+  const items = await readdir(path.join(__dirname, 'files'), {
+    withFileTypes: true,
+  });
+  const files = items.filter((item) => item.isFile());
+  files.forEach((f) => {
+    fs.writeFile(path.join(__dirname, 'files-copy', f.name), '', (err) => {
+      if (err) throw err;
+      const readableStream = fs.createReadStream(
+        path.join(__dirname, 'files', f.name),
+        'utf-8'
+      );
+      readableStream.on('data', (chunk) => {
+        fs.appendFile(
+          path.join(__dirname, 'files-copy', f.name),
+          chunk,
+          (err) => {
+            if (err) throw err;
+          }
+        );
+      });
+    });
+  });
+};
+
+const copyDir = async () => {
+  const dir = await readdir(__dirname);
+  if (!!dir.find((d) => d === 'files-copy')) {
+    fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+      if (err) {
+        throw err;
+      }
+      createDirCopy();
+    });
+  } else {
+    createDirCopy();
+  }
+};
+
+copyDir();
